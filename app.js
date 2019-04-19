ff3ef0cee68c6da53a24a213209ef877a632b4f5
Jaswanth Garre <jasgared@gmail.com>
2019-04-18 22:50:52 -0700

    lists the id's of videos in a playlist

---
+++ app.js
@@ -0,0 +1,32 @@
+const apiKey = "AIzaSyAPy0nrMJoyYQmvCXKUjz8fi8UQKkWs1JM";
+const playlistId = "PLB03EA9545DD188C3";
+
+
+function getLength(e) {
+    let url = "https://www.googleapis.com/youtube/v3/playlistItems?" +
+                "part=snippet,contentDetails" +
+                "&maxResults=50" +
+                "&playlistId=" + playlistId +
+                "&key=" + apiKey;
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        return data.items.map(function (playListItems) {
+            return {
+                videoId: playListItems.contentDetails.videoId,
+                playListItems: playListItems
+            }
+        })
+    }).then(function(arr) {
+        showDetails(arr);
+    }).then(function(error){
+        console.log("Error: " + error);
+    })
+}
+
+function showDetails(data) {
+    data.forEach(function(video){
+        console.log(video.videoId);
+    })
+}
