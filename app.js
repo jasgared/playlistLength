e213c691e6c4733788624c25c65f8d2803a01c66
Jaswanth Garre <jasgared@gmail.com>
2019-04-23 22:50:46 -0700

    extracted duration info of videos

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const apiKey = "your_key_here";
 const playlistId = "PLB03EA9545DD188C3";
 
-
 function getLength(e) {
     let url = "https://www.googleapis.com/youtube/v3/playlistItems?" +
                 "part=snippet,contentDetails" +
@@ -11,7 +10,7 @@ function getLength(e) {
     fetch(url).then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data);
+        //console.log(data);
         return data.items.map(function (playListItems) {
             return {
                 videoId: playListItems.contentDetails.videoId,
@@ -19,14 +18,34 @@ function getLength(e) {
             }
         })
     }).then(function(arr) {
-        showDetails(arr);
-    }).then(function(error){
-        console.log("Error: " + error);
-    })
+            arr.forEach(function (video) {
+                //console.log(video.videoId);
+                let video_url = "https://www.googleapis.com/youtube/v3/videos?" +
+                "part=contentDetails" +
+                "&key=" + apiKey +
+                "&id=" + video.videoId;
+                fetch(video_url).then(function (response) {
+                    return response.json();
+                }).then(function(data) {
+                    console.log(data.items[0].contentDetails.duration);
+                    time = data.items[0].contentDetails.duration;
+                    return time
+                }).catch(function(error) {
+                    console.log("error video: " + error);
+                })
+            })
+        }).catch(function(error) {
+            console.log("Error playlist: " + error);
+        })
 }
 
-function showDetails(data) {
-    data.forEach(function(video){
-        console.log(video.videoId);
-    })
+
+function getTime() {
+    let time = getLength();
+    let hours = 0;
+    let minutes = 0; 
+    let seconds = 0;
 }
+
+
+// wp5sORsPopw
